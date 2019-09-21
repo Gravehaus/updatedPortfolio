62a1c16282a691ff8b10d5987040835b99911cac
juliagraves <gravehaus@gmail.com>
2019-09-21 14:20:23 -0700

    Added new pages with links to each. Added a homepage that ideally will be the lead in to the site however it is a detail and not a necessary one. Will do if I have the time

--- js/js.js
+++ js/js.js
@@ -1,9 +1,10 @@
-//THIS IS THE JQUERY FOR THE CAROUSEL//
-$(document).ready(function(){
-    $('.carousel').carousel();
-  });
-//       -------       -------      //
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
 
 $(document).ready(function(){
-  $('.sidenav').sidenav();
+  $('.carousel').carousel();
 });
